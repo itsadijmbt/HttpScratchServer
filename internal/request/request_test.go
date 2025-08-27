82dd5c0d4ee947a4d25b78bbef49e5b43c9d63dd
itsadijmbt <120321643+itsadijmbt@users.noreply.github.com>
2025-08-27 21:31:08 +0530

    adding request

---
+++ internal/request/request_test.go
@@ -0,0 +1,29 @@
+package request
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRequestLineParse(t *testing.T) {
+
+	r, err := RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	require.Equal(t, "GET", r.RequestLine.Method)
+	require.Equal(t, "/", r.RequestLine.RequestTarget)
+	require.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	r, err = RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	require.Equal(t, "GET", r.RequestLine.Method)
+	require.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+	require.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	_, err = RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+
+}
