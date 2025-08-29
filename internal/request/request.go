d39f982b634075c36b492ae2bb6e6c642c1f87dd
itsadijmbt <120321643+itsadijmbt@users.noreply.github.com>
2025-08-29 21:12:57 +0530

    s3

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,6 +1,10 @@
 package request
 
-import "io"
+import (
+	"fmt"
+	"io"
+	"strings"
+)
 
 type Request struct {
 	RequestLine RequestLine
@@ -12,6 +16,69 @@ type RequestLine struct {
 	Method        string
 }
 
+var ERROR_BAD_START_LINE = fmt.Errorf("bad start line")
+var ERROR_WRONG_START_LINE = fmt.Errorf("malformed start line")
+var ERROR_UNSUPPORTED_HTTP_VERSION = fmt.Errorf("IS NOT A VALID HTTP TYPE\n")
+
+// var BAD_START_LINE = fmt.Errorf("bad start line")
+var Breaker = "\r\n"
+
+func (r *RequestLine) ValidHTTP() bool {
+	return r.HttpVersion == "HTTP/1.1"
+}
+
+func parseRequestLine(b string) (*RequestLine, string, error) {
+	idx := strings.Index(b, Breaker)
+
+	if idx == -1 {
+		return nil, b, nil
+	}
+
+	startline := b[:idx]
+
+	remainingdata := b[idx+len(Breaker):]
+
+	//RFC implements SP i,e single space ! >DSP is a malfunction
+	partsofStartLine := strings.Split(startline, " ")
+
+	httpParts := strings.Split(partsofStartLine[2], "/")
+
+	if len(httpParts) != 2 || httpParts[0] != "HTTP" || httpParts[1] != "1.1" {
+		return nil, remainingdata, ERROR_UNSUPPORTED_HTTP_VERSION
+	}
+
+	if len(partsofStartLine) != 3 {
+		return nil, remainingdata, ERROR_WRONG_START_LINE
+	}
+
+	rl := &RequestLine{
+		Method:        partsofStartLine[0],
+		RequestTarget: partsofStartLine[1],
+		HttpVersion:   httpParts[1],
+	}
+
+	return rl, remainingdata, nil
+}
+
 func RequestFromReader(reader io.Reader) (*Request, error) {
+
+	data, err := io.ReadAll(reader)
+
+	if err != nil {
+		return nil, fmt.Errorf("READER TERMINATED, %w\n", err)
+	}
+	str := string(data)
+
+	rl, _, err := parseRequestLine(str)
+
+	if err !=nil{
+		return  nil, err
+	}
+
+	//after parseReqeust line and validation we recicve the Request
+
+	return &Request{
+		RequestLine: *rl,
+	}, err
 
 }
--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+
 func TestRequestLineParse(t *testing.T) {
 
 	r, err := RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
